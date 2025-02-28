a09bb3874da361f75afd5662b8aad4922e5d9d97
MOHAMMAD RAZA <razamohammad0589@gmail.com>
2025-02-28 23:52:09 +0530

    Add files via upload

---
+++ 3array.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int a[5];
+    a[0]=10;
+    a[1]=20;
+    a[2]=30;
+    a[3]=40;
+    a[4]=50;
+
+    printf("first value is=  %d \n",a[0]);
+    printf("second value is= %d \n",a[1]);
+    printf("third value is=  %d \n",a[2]);
+    printf("foarth value is= %d \n",a[3]);
+    printf("fifth value is=  %d \n",a[4]);
+
+}
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a[5]={1,2,3,4,5};
+//     printf("values are= %d ",a[4]);
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a[2]={1,30};
+//     printf("%d\n%d",a[0],a[1]);
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a[5]={30,40,55};
+//     printf("value is= %d",a[2]);
+// }
